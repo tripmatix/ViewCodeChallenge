2722c3e52eaf0e40a3d7d2b2229bc9af2df1b56b
Romulo Oliveira Viganico <romulooviganico@gmail.com>
2021-12-01 22:39:12 -0300

    criacao de rootview para viewcontroller e primeira label

--- ViewCodeChallenge/ViewController.swift
+++ ViewCodeChallenge/ViewController.swift
@@ -8,10 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Jaber Cagao"
+        label.textColor = .blue
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 35)
+        label.backgroundColor = .systemGray
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+        self.view.addSubview(self.label)
+        configConstraints()
+    }
+    
+    private func configConstraints() {
+        NSLayoutConstraint.activate([
+            self.label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            self.label.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 50),
+            self.label.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -100),
+            self.label.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20),
+            self.label.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20)])
     }
 
 
